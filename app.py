90bc6440da6182a783b2055195289b846238ecb9
Rudrik01 <patelrudrik2004@gmail.com>
2023-12-23 16:32:16 +0530

    add time feature

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template ,url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_migrate import Migrate
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200),nullable=False)
     completed = db.Column(db.Integer,default=0)
     date_created = db.Column(db.DateTime,default=datetime.utcnow)
+    time_created = db.Column(db.Time,default=datetime.now().time())
 
     def __repr__(self):
         return '<Task %r>'% self.id 
     
-    
+
 with app.app_context():
     # Create all tables in the database
     db.create_all()
@@ -38,8 +41,8 @@ def index():
             return 'There was an issue adding your task'
         
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html',tasks=tasks)
+        tasks = Todo.query.order_by(Todo.time_created).all()
+        return render_template('index.html', tasks=tasks)
 
 
 @app.route('/delete/<int:id>')
