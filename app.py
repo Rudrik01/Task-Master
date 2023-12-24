1638a6adc5ede12a28bcc9bea73c7d8373a3ad53
Rudrik01 <patelrudrik2004@gmail.com>
2023-12-24 10:20:24 +0530

    added footer and time feature

--- app.py
+++ app.py
@@ -2,18 +2,27 @@ from flask import Flask, render_template ,url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_migrate import Migrate
+import pytz
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+def get_current_time():
+    return datetime.now(pytz.timezone('Asia/Kolkata')).time()
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200),nullable=False)
     completed = db.Column(db.Integer,default=0)
     date_created = db.Column(db.DateTime,default=datetime.utcnow)
-    time_created = db.Column(db.Time,default=datetime.now().time())
+    time_created = db.Column(db.Time)
+
+
+    def __init__(self, content):
+        self.content = content
+        self.time_created = get_current_time()
 
     def __repr__(self):
         return '<Task %r>'% self.id 
